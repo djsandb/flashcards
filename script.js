a5732f3db9caa6802ee2fe063c50370dd9b4c549
djsandb <sandbothe96@gmail.com>
2021-06-16 06:22:27 -0700

    Add printing

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 class Flashcard {
-	constructor() {
+	constructor(index) {
+		this.index = index;
+
 		const flashcards = document.getElementById('flashcards');
 
 		this.wrapper = document.createElement('div');
@@ -8,6 +10,17 @@ class Flashcard {
 		this.frontEditor = this.addSurface();
 
 		this.backEditor = this.addSurface();
+
+		const deleteLink = document.createElement('a');
+		deleteLink.setAttribute('href', 'javascript:;');
+		deleteLink.setAttribute('class', 'button del');
+		deleteLink.onclick = this.delete.bind(this);
+
+		const deleteIcon = document.createElement('i');
+		deleteIcon.setAttribute('class', 'fas fa-trash fa-2x');
+
+		deleteLink.appendChild(deleteIcon);
+		this.wrapper.appendChild(deleteLink);
 
 		flashcards.appendChild(this.wrapper);
 	}
@@ -22,6 +35,11 @@ class Flashcard {
 			theme: 'bubble'
 		});
 	}
+
+	delete() {
+		this.wrapper.remove();
+		gFlashcards.removeCard(this.index);
+	}
 }
 
 class FlashcardSet {
@@ -30,7 +48,10 @@ class FlashcardSet {
 		this.addCard();
 	}
 	addCard() {
-		this.cards.push(new Flashcard());
+		this.cards.push(new Flashcard(this.cards.length));
+	}
+	removeCard(index) {
+		this.cards.splice(index, 1);
 	}
 }
 
@@ -40,4 +61,92 @@ function AddCard()
 {
 	gFlashcards.addCard();
 	window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
+}
+
+function GetPrintHtml()
+{
+	let ret = `
+		<!doctype html>
+		<head>
+	  		<meta charset="utf-8">
+			<style>
+				* {
+					box-sizing: border-box; 
+				}
+
+				@page {
+					margin: 0;
+					background: white;
+				}
+				
+				body {
+					margin: 0;
+				}
+
+				.container {
+					width: 10in;
+					height: 6in;
+					margin-left: 0.5in;
+					margin-top: 1.25in;
+					overflor: hidden;
+					display: flex;
+					flex-wrap: wrap;
+					page-break-after: always;
+				}
+
+				p {
+					margin: 0;
+				}
+				
+				.flashcard {
+					width: 50%;
+					height: 3in;
+					background: white;
+					border: 1px solid black;
+					padding: 12px 15px;
+					font-family: Helvetica, Arial, sans-serif;
+					font-size: 13px;
+					tab-size: 4;
+					white-space: pre;
+				}
+
+				.pagebreak {
+					page-break-before: always;
+					margin-bottom: 0.5in;
+				}
+			</style>
+		</head>
+	
+		<body>
+	`;
+
+	let nSets = Math.ceil(gFlashcards.cards.length / 4);
+
+	for (let set = 0; set < nSets; set++)
+	{
+		let startIndex = 4 * set;
+		let nCards = Math.min(4, gFlashcards.cards.length - (4 * set));
+
+		ret += '<div class="container">';
+		for (let i = startIndex; i < startIndex + nCards; i++) {
+			ret += '<div class="flashcard">' + gFlashcards.cards[i].frontEditor.root.innerHTML + '</div>\n';
+		}
+		ret += '</div>';
+
+		ret += '<div class="container">';
+		for (let i = startIndex; i < startIndex + nCards; i++) {
+			ret += '<div class="flashcard">' + gFlashcards.cards[i].backEditor.root.innerHTML + '</div>\n';
+		}
+		ret += '</div>';
+	}
+
+	ret += '</body></html>'
+
+	return ret;
+}
+
+function PrintCards()
+{
+	var newWin = window.open();
+	newWin.document.write(GetPrintHtml());
 }
